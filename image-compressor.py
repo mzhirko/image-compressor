ab1a824a3ff4d3becb14a765b1e0b1ab9759ca93
Alex Khalupka <alexkha2001@gmail.com>
2020-11-12 03:25:45 +0300

    learning added

--- image-compressor.py
+++ image-compressor.py
@@ -41,3 +41,59 @@ W_first = np.empty((N, p))
 W_second = np.empty((p, N))
 X_out = np.empty((L, N))
 X_delta = np.empty((L, N))
+
+p = N // 3
+
+print((N * L) / ((N + L) * p + 2))
+
+# adding axis to array (for easier matrix transposing)
+Xq = np.expand_dims(Xq, axis=1)
+X_out = np.expand_dims(X_out, axis=1)
+X_delta = np.expand_dims(X_delta, axis=1)
+
+# fielding weight matrices with random values
+random.seed()
+for i in range(N):
+    W_first[i] = np.random.uniform(-1, 1, p)
+
+W_second = np.transpose(W_first)
+
+iteration_counter = 0
+E = 10000000
+while E > 1000:
+    E = 0
+
+    for k in range(L):
+        # counting Y
+        Y = np.dot(Xq[k], W_first)
+
+        # Counting X'
+        X_out[k] = np.dot(Y, W_second)
+
+        # Counting delta X
+        X_delta[k] = X_out[k] - Xq[k]
+
+        # Counting W'
+        alpha_second = adaptive_learning_step(Y)
+        # print(alpha_second)
+        W_second = W_second - alpha_second * np.dot(np.transpose(Y), X_delta[k])
+
+        # Counting W
+        alpha_first = adaptive_learning_step(Xq[k])
+        # print(alpha_first)
+        W_first = W_first - alpha_first * np.dot(np.dot(np.transpose(Xq[k]), X_delta[k]),  np.transpose(W_second))
+
+        Y = np.dot(Xq[k], W_first)
+        X_out[k] = np.dot(Y, W_second)
+        X_delta[k] = X_out[k] - Xq[k]
+
+        # counting error
+        for i in range(N):
+            E += X_delta[k][0][i] * X_delta[k][0][i]
+
+    # increasing iteration counter
+
+    iteration_counter += 1
+    print("Iteration number: {}, error {}".format(iteration_counter, E))
+
+print("Final iteration count: {}, final error {}".format(iteration_counter, E))
