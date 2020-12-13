428f6706e935f80109a223544c6e3a418e7941f7
mzhirko <mzhirko@gmail.com>
2020-12-13 16:41:25 +0300

    Add authors

--- image-compressor.py
+++ image-compressor.py
@@ -1,3 +1,9 @@
+# Linear Recirculation Network Model With Adaptive Learning Step and Normalized Weights
+# done by: Холупко А.А, Жирко М. С.
+# st. of gr.: 821701
+
+# Холупко А.А.
+
 import math
 import random
 import numpy as np
@@ -9,11 +15,7 @@ from PIL import Image
 # def normalize_matrix counts adaptive learning step
 def adaptive_learning_step(matrix):
     tmp = np.dot(matrix, np.transpose(matrix))
-    s = 0
-    for i_f in range(len(tmp)):
-        s += tmp[i_f]
-
-    return 1.0 / (s * 10)
+    return 1.0 / (tmp * 10)
 
 
 # def normalize_matrix normalizes given matrix
@@ -27,20 +29,21 @@ def normalize_matrix(matrix):
             matrix[j_f][i_f] = matrix[j_f][i_f] / s
 
 
+# Мария Жирко
 # loading image
-image = Image.open("mrz.jpg")
+image = Image.open("goya.jpg")
 image.show()
 pix = image.load()
 H = image.height
 W = image.width
-print("Result for mrz.jpg {}x{}".format(H, W))
+print("Result for finally.jpg {}x{}".format(H, W))
 
 
 # setting image parameters
 
 S = 3
 c_max = 255
-n = m = 8
+n = m = 4
 N = n * m * S
 L = int(H / n * W / m)
 Xq = []
@@ -60,11 +63,11 @@ for h in range(0, H, n):
 
 
 # setting variables
-p = N // 4
-e = p * 150
+p = N // 2
 
 print((N * L) / ((N + L) * p + 2))
 
+# Холупко Александр
 # initialize matrices
 W_first = np.empty((N, p))
 W_second = np.empty((p, N))
@@ -84,8 +87,8 @@ for i in range(N):
 W_second = np.transpose(W_first)
 
 iteration_counter = 0
-E = 1000000
-while E > 100:
+E = 10000000
+while E > 0:
     E = 0
 
     for k in range(L):
@@ -98,25 +101,30 @@ while E > 100:
         # Counting delta X
         X_delta[k] = X_out[k] - Xq[k]
 
+        # Counting W
+        alpha_first = adaptive_learning_step(Xq[k])
+        # print(alpha_first)
+        W_first = W_first - alpha_first * np.dot(np.dot(np.transpose(Xq[k]), X_delta[k]), np.transpose(W_second))
+
         # Counting W'
         alpha_second = adaptive_learning_step(Y)
         # print(alpha_second)
         W_second = W_second - alpha_second * np.dot(np.transpose(Y), X_delta[k])
 
-        # Counting W
-        alpha_first = adaptive_learning_step(Xq[k])
-        # print(alpha_first)
-        W_first = W_first - alpha_first * np.dot(np.dot(np.transpose(Xq[k]), X_delta[k]),  np.transpose(W_second))
-
         # normalizing matrices
         normalize_matrix(W_first)
         normalize_matrix(W_second)
 
-        # Y = np.dot(Xq[k], W_first)
-        # X_out[k] = np.dot(Y, W_second)
-        # X_delta[k] = X_out[k] - Xq[k]
+    for k in range(L):
+        # counting Y
+        Y = np.dot(Xq[k], W_first)
 
-        # counting error
+        # Counting X'
+        X_out[k] = np.dot(Y, W_second)
+
+        # Counting delta X
+        X_delta[k] = X_out[k] - Xq[k]
+
         for i in range(N):
             E += X_delta[k][0][i] * X_delta[k][0][i]
 
@@ -129,6 +137,7 @@ print("Final iteration count: {}, final error {}".format(iteration_counter, E))
 
 
 # Compressing and restoring image on counted weights
+# Мария Жирко
 for k in range(L):
     Y = np.dot(Xq[k], W_first)
     X_out[k] = np.dot(Y, W_second)
@@ -149,7 +158,7 @@ for h in range(0, H, n):
         for j in range(n):
             for k in range(m):
                 for i in range(S):  # rgb
-                    image_restored[j + h, k + w, i] = (x_out[i + S * (j + k * n)] + 1) * c_max / 2
+                    image_restored[j + h, k + w, i] = (x_out  [i + S * (j + k * n)] + 1) * c_max / 2
                     image_origin[j + h, k + w, i] = (xq[i + S * (j + k * n)] + 1) * c_max / 2
 
 # compression ratio
